acfe77ed36bb3cd0d3e622c04bcd8375bf26f6f1
TheNazik <thena3ik@gmail.com>
2026-07-07 19:25:43 +0300

    Changed ProductResponse price from Double to BigDecimal for consistency

--- src/main/java/com/thena3ik/shopapi/dto/product/ProductResponse.java
+++ src/main/java/com/thena3ik/shopapi/dto/product/ProductResponse.java
@@ -1,12 +1,14 @@
 package com.thena3ik.shopapi.dto.product;
 
+import java.math.BigDecimal;
+
 public record ProductResponse(
         Long id,
         String sku,
         String name,
         String brand,
         String description,
-        Double price,
+        BigDecimal price,
         Integer stockQuantity,
         Long categoryId,
         String categoryName) {
